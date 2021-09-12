d5fefbc851ce8a977a46b4e95bd54732cf6b18df
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2021-09-12 12:33:14 -0700

    Initial commit

---
+++ commands/8ball.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const config = require('./config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('8ball')
+		.setDescription('Ask the magic 8ball...')
+		.addStringOption(option =>
+			option.setName('question')
+				.setDescription('Question to ask the 8 ball')
+				.setRequired(true)),
+	async execute(interaction) {
+		const result = Math.floor((Math.random() * config.responses.length));
+
+		const question = args.join(' ');
+
+		const embed = new Discord.MessageEmbed()
+			.setTitle('8-Ball')
+			.addField('**Question**', question, true)
+			.addField('**Answer**', config.responses[result])
+			.setThumbnail(message.author.avatarURL())
+			.setTimestamp(message.createdAt)
+			.setFooter('FartBot2000 | !help', message.client.user.avatarURL());
+		if (result === 0 || result === 1 || result === 2 || result === 3 || result === 4 || result === 5 || result === 6 || result === 7 || result === 8 || result === 9) {
+			embed.setColor('#39ff14');
+		} else if (result === 10 || result === 11 || result === 12 || result === 13 || result === 14) {
+			embed.setColor('#ffff00');
+		} else {
+			embed.setColor('#ff0000');
+		}
+
+		await interaction.reply({ embeds: embed });
+	},
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+// FartBot2000-v13
+// Updating FartBot2000 to discord.js
+// ----------------------------------
+// Require necessary packages
+const { Client, Collection, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// Create client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Reading command files
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+// Configure dotenv
+dotenv.config();
+
+// Once the client is ready, this will run (once)
+client.once('ready', () => {
+	console.log('Hello.');
+});
+
+// When an interaction is created, this will run
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: '❌ There was an error trying to execute this command :(', ephemeral: true });
+	}
+});
+
+// Login to bot
+client.login(process.env.TOKEN);
