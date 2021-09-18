146ddf1f63a7ba14340e82f3da8741cac9ad6e19
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2021-09-17 20:51:28 -0700

    Update clear command

---
+++ commands/clear.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('Deletes messages from a channel')
+		.addIntegerOption(option =>
+			option.setName('amount')
+				.setDescription('Amount of messages to delete')
+				.setRequired(true)),
+	async execute(interaction) {
+		if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+			return interaction.reply({ content: '❌ Sorry kitten you don\'t have admin tho ☹', ephemeral: true });
+		}
+
+		const amount = interaction.options.getInteger('amount');
+		const channel = interaction.channel;
+
+		await channel.bulkDelete(amount, true);
+
+		const embed = new MessageEmbed()
+			.setTitle('Done!')
+			.setColor('GREEN')
+			.setDescription(`Successfully deleted ${amount} message(s) in ${channel}.`)
+			.setThumbnail(interaction.user.avatarURL())
+			.setTimestamp(interaction.createdAt)
+			.setFooter('FartBot2000 | !help', interaction.client.user.avatarURL());
+
+		await interaction.reply({ embeds: [embed] });
+	},
+};
